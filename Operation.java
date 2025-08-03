f63bc266257f4d82698d8cbfe3eca51ab201c674
Rahul Chaudhary <rahulchaudhary4824@gmail.com>
2025-08-03 22:45:34 +0530

    first commit

---
+++ Operation.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Operation {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Sum = " + (a+b));
+        System.out.println("Subtract = " + (a-b));
+        System.out.println("Multiplication = " + (a*b));
+        System.out.println("Division = " + (a/b));
+        System.out.println("Modulus = " + (a%b));
+    }
+}

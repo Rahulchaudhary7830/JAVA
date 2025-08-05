7553365757f443e0f094fda2311895cd6db221c0
Rahul Chaudhary <rahulchaudhary4824@gmail.com>
2025-08-05 23:14:25 +0530

    code

---
+++ Palindrome.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter number");
+        int num=sc.nextInt();
+        int temp=num;
+        int sum=0;
+        while(num>0){
+            int digit=num%10;
+            sum=sum*10+digit;
+            num=num/10;
+        }
+        if(sum==temp){
+            System.out.println("It is palindrome");
+        }
+        else{
+        System.out.println("It is not palindrome"); 
+        }
+        sc.close(); 
+    }
+}

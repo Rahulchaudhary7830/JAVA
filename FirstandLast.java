a7b539f1db7e26d18f9f6dd3a57d21c6e16d248a
Rahul Chaudhary <chrahul7830@gmail.com>
2025-08-22 00:07:03 +0530

    Update

--- FirstandLast.java
+++ FirstandLast.java
@@ -1,55 +1,60 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class FirstandLast {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter size of array");
-    int n= sc.nextInt();
-    System.out.println("Enter array elements");
-    int [] arr = new int[n];
-    for(int i=0;i<arr.length;i++){
-      arr[i] = sc.nextInt();    
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter size of array");
+        int n = sc.nextInt();
+        System.out.println("Enter array elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target value");
+        int target = sc.nextInt();
+        int[] idx = searchRange(arr, target);
+        System.out.println("Target present at indices: " + Arrays.toString(idx));
+        sc.close();
     }
-    System.out.println("Enter target value");
-    int target = sc.nextInt();
-    int [] idx = searchRange(arr,target);
-    for(int i=0;i<idx.length;i++){
-    System.out.println(" target present at index: "+idx[i]);}
-    sc.close();
-    }
+
     public static int[] searchRange(int[] arr, int target) {
-        int s=0;
-        int e=arr  .length-1;
         int[] ans = new int[2];
-        ans[0]=-1;
-        ans[1]=-1;
-        while(s<=e){
-            int mid = (s+e)/2;
-            if(arr[mid]==target){
-                ans[0]=mid;
-                e=mid-1;
-            }
-            else if(arr[mid]<target){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-        }
-        s=0;
-        e=arr.length-1;
-        while(s<=e){
-            int mid =(s+e)/2;
-            if(arr[mid]==target){
-                ans[1]=mid;
-                s=mid+1;
-            }
-            else if(arr[mid]<target){
-                s=mid+1;
-            }
-            else{
-                e=mid-1;
-            }
-        }
+        ans[0] = firstOccurrence(arr, target);
+        ans[1] = lastOccurrence(arr, target);
         return ans;
-    }    
+    }
+
+    public static int firstOccurrence(int[] arr, int target) {
+        int s = 0, e = arr.length - 1;
+        int res = -1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (arr[mid] == target) {
+                res = mid;
+                e = mid - 1;
+            } else if (arr[mid] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return res;
+    }
+
+    public static int lastOccurrence(int[] arr, int target) {
+        int s = 0, e = arr.length - 1;
+        int res = -1;
+        while (s <= e) {
+            int mid = (s + e) / 2;
+            if (arr[mid] == target) {
+                res = mid;
+                s = mid + 1; 
+            } else if (arr[mid] < target) {
+                s = mid + 1;
+            } else {
+                e = mid - 1;
+            }
+        }
+        return res;
+    }
 }
